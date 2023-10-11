2327a29fde0aa2fe75e0d6c421cd674b202fce4d
LuanaAbramoff <62798202+LuanaAbramoff@users.noreply.github.com>
2023-10-11 16:44:58 -0300

    tudo_feitoooooooooooooooooooooooooooo

--- RTOS-OLED-Xplained-Pro/src/main.c
+++ RTOS-OLED-Xplained-Pro/src/main.c
@@ -20,6 +20,103 @@
 #define BTN_PIO_PIN_MASK (1 << BTN_PIO_PIN)
 
 
+#define BUZZER_PIO	PIOA
+#define BUZZER_PIO_ID ID_PIOA
+#define BUZZER_PIO_IDX 5
+#define BUZZER_PIO_IDX_MASK (1 << BUZZER_PIO_IDX)
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+
 
 /************************************************************************/
 /* prototypes and types                                                 */
@@ -27,6 +124,8 @@
 
 void btn_init(void);
 void RTT_init(float freqPrescale, uint32_t IrqNPulses, uint32_t rttIRQSource);
+void RTT_Handler(void);
+void tone (int freq, int duration);
 
 /************************************************************************/
 /* rtos vars                                                            */
@@ -42,6 +141,9 @@ QueueHandle_t xQueueCoins;
 
 #define TASK_COINS_STACK_SIZE                (1024*6/sizeof(portSTACK_TYPE))
 #define TASK_COINS_STACK_PRIORITY            (tskIDLE_PRIORITY)
+
+#define TASK_PLAY_STACK_SIZE                (1024*6/sizeof(portSTACK_TYPE))
+#define TASK_PLAY_STACK_PRIORITY            (tskIDLE_PRIORITY + 1)
 
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,  signed char *pcTaskName);
 extern void vApplicationIdleHook(void);
@@ -82,17 +184,61 @@ static void task_coins (void *pvParameters){
 	
 	
 	int coins;
-	
+	double tempo;
+	int flag = 0;
+	RTT_init(1000, 5, RTT_MR_ALMIEN);
 	
 	for(;;){
-		if( xSemaphoreTake(xBtnSemaphore, 500 / portTICK_PERIOD_MS) == pdTRUE ){
-			//int coins = (rand() % 3) + 1;
-			double tempo = rtt_read_timer_value(RTT);
-			printf("%lf",tempo);
+		
+		
+		if( xSemaphoreTake(xBtnSemaphore, 200) == pdTRUE ){
+			if (flag == 0){
+				tempo = rtt_read_timer_value(RTT);
+				flag = 1;
+				srand(tempo);
+				printf("Seed: %lf \n",tempo);
+			}
+			
+			coins = (rand() % 3) + 1;
+			
+			xQueueSend(xQueueCoins, &coins, 1000);
+		}
+		
+			
+
+	}
+	
+}
+
+static void task_play (void *pvParameters){
+	int coins;
+	
+	for(;;){
+		if( xQueueReceive( xQueueCoins, &coins, ( TickType_t ) 500 )){
+			printf("Coins: %d \n",coins);
+			if(coins == 1){
+				tone(NOTE_B5, 80);
+				tone(NOTE_E6, 640);
+			}
+			if(coins == 2){
+				tone(NOTE_B5, 80);
+				tone(NOTE_E6, 640);
+				tone(NOTE_B5, 80);
+				tone(NOTE_E6, 640);
+			}
+			if(coins == 3){
+				tone(NOTE_B5, 80);
+				tone(NOTE_E6, 640);
+				tone(NOTE_B5, 80);
+				tone(NOTE_E6, 640);
+				tone(NOTE_B5, 80);
+				tone(NOTE_E6, 640);
+			}
 		}
 	}
 	
 }
+
 
 
 static void task_debug(void *pvParameters) {
@@ -116,12 +262,15 @@ static void task_debug(void *pvParameters) {
 void btn_init(void) {
 	// Inicializa clock do periférico PIO responsavel pelo botao
 	pmc_enable_periph_clk(BTN_PIO_ID);
-
+	pmc_enable_periph_clk(BUZZER_PIO_ID);
+	
 	// Configura PIO para lidar com o pino do botão como entrada
 	// com pull-up
 	pio_configure(BTN_PIO, PIO_INPUT, BTN_PIO_PIN_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_configure(BUZZER_PIO, PIO_OUTPUT_0, BUZZER_PIO_IDX_MASK, PIO_DEFAULT);
 	pio_set_debounce_filter(BTN_PIO, BTN_PIO_PIN_MASK, 60);
 	
+	pio_set_output(BUZZER_PIO, BUZZER_PIO_IDX_MASK, 0, 0, 0);
 	// Configura interrupção no pino referente ao botao e associa
 	// função de callback caso uma interrupção for gerada
 	// a função de callback é a: but_callback()
@@ -170,6 +319,16 @@ void RTT_init(float freqPrescale, uint32_t IrqNPulses, uint32_t rttIRQSource) {
 	rtt_disable_interrupt(RTT, RTT_MR_RTTINCIEN | RTT_MR_ALMIEN);
 }
 
+void RTT_Handler(void) {
+	uint32_t ul_status;
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		
+	}
+}
+
 static void configure_console(void) {
 	const usart_serial_options_t uart_serial_options = {
 		.baudrate = CONF_UART_BAUDRATE,
@@ -185,6 +344,20 @@ static void configure_console(void) {
 	setbuf(stdout, NULL);
 }
 
+void tone (int freq, int duration){
+	int t = (1000000/(2*freq));
+	int ciclo = duration*1000/(2*t);
+	double ciclo_led = 0.75*ciclo;
+
+	for (int i = 0; i<ciclo; i++){
+
+		pio_clear(BUZZER_PIO,BUZZER_PIO_IDX_MASK);
+		delay_us(t);
+		pio_set(BUZZER_PIO,BUZZER_PIO_IDX_MASK);
+		delay_us(t);
+	}
+	
+}
 /************************************************************************/
 /* main                                                                 */
 /************************************************************************/
@@ -198,6 +371,8 @@ int main(void) {
 	
 	/* Inicializar botão */
 	btn_init();
+	
+	
 	
 	// cria semáforo binário
 	xBtnSemaphore = xSemaphoreCreateBinary();
@@ -225,7 +400,11 @@ int main(void) {
 		printf("Failed to create coins task\r\n");
 	}
 	
-	RTT_init(1000, 5,RTT_MR_ALMIEN);
+	if (xTaskCreate(task_play, "play", TASK_PLAY_STACK_SIZE, NULL, TASK_PLAY_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create coins task\r\n");
+	}
+	
+	
 
 	/* Start the scheduler. */
 	vTaskStartScheduler();
